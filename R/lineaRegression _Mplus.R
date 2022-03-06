c0d9b7ea1f7ae41e6afb48551741d6eb75c8465b
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2022-03-05 23:59:54 -0500

    Add MplusGLM

--- R/lineaRegression _Mplus.R
+++ R/lineaRegression _Mplus.R
@@ -8,13 +8,13 @@ SAPNS_df <- rename(SAPNS_df, c('CP1SAPS' = 'CP1_SAPS'))
 # binary var: ref cat = 1st cat
 # multinomial var: ref cat = last cat
 
-model <- mplusObject( #To replace with mplusGLM()
+model <- mplusObject(
   TITLE = 'Linear regression;',
-  MODEL = 'FIQ ON K_SAPS;',
+  MODEL = 'K_SAPS ON FIQ; FIQ;', #need pred>1 or cite pred in model to use available information
   OUTPUT = c('sampstat standardized tech1;'),
   ANALYSIS = 'ESTIMATOR = MLR;',
   usevariables = usevar,
-  rdata = SAPNS_df
+  rdata = SAPScov_df
 )
 
 results <- mplusModeler(
@@ -28,5 +28,18 @@ results <- mplusModeler(
   writeData ="always"
 )
 
+SAPScov_df <-  SAPScov_df %>% mutate(K_SAPS=factor(K_SAPS))
+mplusGLM <-  mplusGLM(K_SAPS ~ FIQ, SAPScov_df, 'pin')
+# = #
+MplusAutomation:::.mplusMultinomial(
+  'K_SAPS',
+  'FIQ',
+  SAPScov_df,
+  idvar = 'pin',
+  integration = 1000,
+  processors = 16,
+  OR = TRUE,
+  pairwise = TRUE,
+)
 
 
