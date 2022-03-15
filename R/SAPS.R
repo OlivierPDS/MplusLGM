294862731e6d11afac942813c80c5c2f0e4627f5
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2022-03-15 12:28:20 -0400

    Update, reorganize SAPS
    TSCORES
    GMM
    CSV files
    variables of interest

--- R/SAPS.R
+++ R/SAPS.R
@@ -1,7 +1,4 @@
-# MplusLGM
-
-## Installation
-
+### Installation
 # Install devtools from CRAN if not already installed 
 install.packages("devtools")
 
@@ -13,33 +10,78 @@ BiocManager::install("rhdf5")
 # Install the latest version of MplusLGM from this repository
 devtools::install_github("joshunrau/MplusLGM")
 
-
-### Step 1: Load the Package and Dataset
-
+### Step 0: Load the Package and Dataset
 # Load required packages
 library(MplusLGM)
 library(tidyverse)
 library(haven)
 library(MplusAutomation)
+library(magrittr)
+library(glue)
+library(lubridate)
+
+# Some Mplus Language:
+    # vars: refers to variances and residual variances; ex: var1 var1-var9;
+    # [vars]: refers to means, intercepts, thresholds; ex: [var1, var1-var9];
+    # *: frees a parameter at a default value or a specific starting value; ex:  var1* var2*.5;
+    # @: fixes a parameter at a default value or a specific value; ex: var1@ var2@0;
+    # (number): constrains parameters to be equal var1(1) var2(1)
 
 # Load dataset
+SAPS <- str_c('SAPS_', c(0, 1, 2, 3, 6, 9, 12, 18, 24))
+t <-  str_c('t', c(0, 1, 2, 3, 6, 9, 12, 18, 24))
+
 SAPS_df <- 
-  paste('/Users/olivierpercie/OneDrive - McGill University/CRISP_Lab/LTOS/Data/Datasets/PEPP2/PEPP2_2022-02-28.sav') %>% 
-  read_spss(user_na = FALSE, skip = 0, n_max = Inf) %>%
+  paste('/Users/olivierpercie/OneDrive - McGill University/CRISP_Lab/LTOS/Data/Datasets/PEPP2/PEPP2_2022-03-11.csv') %>% 
+  read_csv() %>%
   subset(miss_SAPS <= 4 & n == 1) %>% 
-  select ('pin', all_of(c(SAPS)))
+  select ('pin', all_of(c(SAPS, t)))
 
-
-# Examine the structure of the dataset
-str(df)
-
-# Get means for each diagnostic group at variables of interest
+# Get means for each diagnostic group at variables of interest 
 df %>% group_by(dx) %>% 
   summarise_at(vars(colnames(df)[3:9]), mean, na.rm = TRUE)
 
 
-### Step 2: Group-Based Trajectory Modeling
+### Step 1: Growth Curve Modeling
 
+# s@0; !set slope factor variance to 0
+# [s@0]; !set slope factor mean to 0
+# i with s@0; !set growth factor covariance to 0
+# vars (1); fix residual variance
+
+#Run GCM model
+gcm_models <- mplusObject(
+  TITLE = 'GCM_P3_K1_S1000;',
+  VARIABLE = c(
+    'NAMES = pin SAPS_0 SAPS_1 SAPS_2 SAPS_3 SAPS_6 SAPS_9 SAPS_12 SAPS_18 SAPS_24;',
+    'MISSING=.;',
+    'USEVAR = SAPS_0-SAPS_24;'),
+  MODEL = 'i s q cub | SAPS_0@0 SAPS_1@1 SAPS_2@2 SAPS_3@3
+    SAPS_6@6 SAPS_9@9 SAPS_12@12 SAPS_18@18 SAPS_24@24;',
+  ANALYSIS = c(
+    'TYPE = GENERAL;',
+    'PROCESSORS = 16;'),
+  OUTPUT = 'TECH1 SAMPSTAT STANDARDIZED;',
+  SAVEDATA ='
+    file = K1_S500_res.dat;',
+  autov = FALSE,
+  rdata = SAPS_df 
+)
+
+# Create, run, and read Mplus models
+FitIndices_gcm <- mplusModeler(
+  object = gcm_models,
+  dataout = str_c(getwd(),'/SAPS/Results/GCM/GCM_P3_K1.dat'),
+  modelout = str_c(getwd(),'/SAPS/Results/GCM/GCM_P3_K1.inp'),
+  hashfilename = FALSE,
+  run = 1,
+  check=FALSE,
+  writeData ="always"
+)
+
+#add GCM fit indices
+
+### Step 2: Group-Based Trajectory Modeling
 # Run GBTM models
 gbtm_models <- fitGBTM(
   df = SAPS_df,
@@ -50,15 +92,17 @@ gbtm_models <- fitGBTM(
   max_k = 6)
 
 # Examine fit indices
-FitIndices_gbtm <- getFitIndices(gbtm_models)
+# - N > 176:  CAIC > BIC > aBIC > AIC
+# - entropy > 0.8: CAIC & BIC > aBIC & BLRT  
+# - class count should not be < 25% sample size
+# - H0:K=K1-1 H1:K=K1 If likelihood ratio p<0.05 then choose K1, else choose K1-1
 
-# class count should not be < 25% sample size
+FitIndices_gbtm <- getFitIndices(gbtm_models)
 
 #best_gbtm_model <- selectBestModel(gbtm_models, selection_method = "BIC_LRT") # to improve
 best_gbtm_model <- gbtm_models[[2]]
 
 ### Step 3: Examine Alternative Variance Structures
-
 # Run LCGA models
 lcga_models <- fitLCGA(
   df = SAPS_df,
@@ -76,7 +120,6 @@ FitIndices_lcga <- getFitIndices(lcga_models)
 best_bic_model <- selectBestModel(lcga_models, selection_method = "BIC")
 
 ### Step 4: Refine Polynomial Order
-
 final_model <- refinePolynomial(
   model = best_bic_model, 
   df = SAPS_df,
@@ -85,7 +128,10 @@ final_model <- refinePolynomial(
   working_dir = paste(getwd(), 'SAPS', sep = '/'),
   idvar = "pin")
 
+# Examine fit indices
 FitIndices_final_model <- final_model %>% list() %>% getFitIndices()
+
+### Step 5: Extract model parameters and save results
 
 #Get class counts & proportions of all models (to improve)
 library(plyr)
@@ -101,14 +147,14 @@ write_excel_csv(mostlikely, paste(getwd(), 'SAPS', 'SAPSclass_counts', sep = '/'
 SAPSclass_counts <- read_excel(paste(getwd(), 'SAPS', 'SAPSclass_counts', sep = '/'))
 wide_mostlikely <- pivot_wider(xl, names_from = 'class', values_from = c('count', 'proportion'))
 
-### Step 5: Plot trajectories
-
 # Get and save final dataset based on most probable class membership
 final_dataset <- getDataset(final_model, SAPS405_df, 'pin')
 
 data <- data.frame(final_model[["results"]][["savedata"]]) %>% rename(pin = PIN)
 final_dataset <- merge(SAPS405_df, data, by = 'pin') %>% select('pin','C', starts_with('CPROB'))
 write_sav(final_dataset, paste(getwd(), 'SAPS', 'SAPS405.sav', sep = '/'))
+
+### Step 6: Plot trajectories
 
 # Get means as long form
 class_means <- getLongMeans(
@@ -153,38 +199,236 @@ est.means <-
   final_model[["results"]][["gh5"]][["means_and_variances_data"]][["y_estimated_medians"]][["values"]] %>%
   as.data.frame()
 
-#[Alt Text](https://github.com/joshunrau/MplusLGM/blob/main/example/adv_plot.png?raw=true)
+### Step x: Growth Mixture Models
+  # GMM1 - equality across t and k 
+gmm1_models <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  gmm1_models[[g]] <- mplusObject(
+    TITLE = glue('GMM1_{g};'),
+    VARIABLE = c(
+      'NAMES = pin SAPS_0 SAPS_1 SAPS_2 SAPS_3 SAPS_6 SAPS_9 SAPS_12 SAPS_18 SAPS_24;',
+      'MISSING=.;',
+      'USEVAR = SAPS_0-SAPS_24;',
+      'CLASSES = c(2);'),
+    MODEL = c(
+      '%OVERALL%',
+      'i s q cub | SAPS_0@0 SAPS_1@1 SAPS_2@2 SAPS_3@3 SAPS_6@6 
+                   SAPS_9@9 SAPS_12@12 SAPS_18@18 SAPS_24@24;',
+      glue('{g};'),
+      'SAPS_0-SAPS_24 (1);'),
+    ANALYSIS = c(
+      'TYPE = MIXTURE;',
+      'STARTS = 1000 250;',
+      'PROCESSORS = 16;'),
+    OUTPUT = 'SAMPSTAT STANDARDIZED TECH1 TECH11;',
+    SAVEDATA = glue('file = GMM1_{g}_res.dat;'),
+    autov = FALSE,
+    rdata = SAPS_df 
+  )
+}
 
+# Create, run, and read Mplus models
+FitIndices_gmm1 <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  
+  FitIndices_gmm1[[g]] <- mplusModeler(
+    object = gmm1_models[[g]],
+    dataout = glue(getwd(),'/SAPS/Results/GMM/GMM1/GMM1_{g}.dat'),
+    modelout = glue(getwd(),'/SAPS/Results/GMM/GMM1/GMM1_{g}.inp'),
+    hashfilename = FALSE,
+    run = 1,
+    check=TRUE,
+    varwarnings = TRUE,
+    writeData ="always"
+  )
+}
 
-### Using individually varying timescores  
+  # GMM2 - equality across t/free across k 
+gmm2_models <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  gmm2_models[[g]] <- mplusObject(
+    TITLE = glue('GMM2_{g};'),
+    VARIABLE = c(
+      'NAMES = pin SAPS_0 SAPS_1 SAPS_2 SAPS_3 SAPS_6 SAPS_9 SAPS_12 SAPS_18 SAPS_24;',
+      'MISSING=.;',
+      'USEVAR = SAPS_0-SAPS_24;',
+      'CLASSES = c(2);'),
+    MODEL = c(
+      '%OVERALL%',
+      'i s q cub | SAPS_0@0 SAPS_1@1 SAPS_2@2 SAPS_3@3 SAPS_6@6 
+                   SAPS_9@9 SAPS_12@12 SAPS_18@18 SAPS_24@24;',
+      glue('{g};'),
+      
+      '%c#1%',
+      'SAPS_0-SAPS_24 (1);',
+      
+      '%c#2%',
+      'SAPS_0-SAPS_24 (2);'),
+    ANALYSIS = c(
+      'TYPE = MIXTURE;',
+      'STARTS = 1000 250;',
+      'PROCESSORS = 16;'),
+    OUTPUT = 'SAMPSTAT;',
+    SAVEDATA = glue('file = GMM2_{g}_res.dat;'),
+    autov = FALSE,
+    rdata = SAPS_df 
+  )
+}
 
-SAPS_df <- SAPS_df %>% select ('pin', all_of(c(SAPS, t)))
+# Create, run, and read Mplus models
+FitIndices_gmm2 <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  
+  FitIndices_gmm2[[g]] <- mplusModeler(
+    object = gmm2_models[[g]],
+    dataout = glue(getwd(),'/SAPS/Results/GMM/GMM2/GMM2_{g}.dat'),
+    modelout = glue(getwd(),'/SAPS/Results/GMM/GMM2/GMM2_{g}.inp'),
+    hashfilename = FALSE,
+    run = 1,
+    check=TRUE,
+    varwarnings = TRUE,
+    writeData ="always"
+  )
+}
 
-# Exclude cases with complete SAPS and missing date of observation
-SAPS_df <- SAPS_df %>%
-  filter(xor(!is.na(SAPS_1), is.na(t1))) %>% 
-  filter(xor(!is.na(SAPS_2), is.na(t2))) %>% 
-  filter(xor(!is.na(SAPS_3), is.na(t3))) %>% 
-  filter(xor(!is.na(SAPS_6), is.na(t6))) %>% 
-  filter(xor(!is.na(SAPS_9), is.na(t9))) %>% 
-  filter(xor(!is.na(SAPS_12), is.na(t12))) %>% 
-  filter(xor(!is.na(SAPS_18), is.na(t18))) %>% 
+  # GMM3 - equality across k/free across t 
+gmm3_models <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  gmm3_models[[g]] <- mplusObject(
+    TITLE = glue('GMM3_{g};'),
+    VARIABLE = c(
+      'NAMES = pin SAPS_0 SAPS_1 SAPS_2 SAPS_3 SAPS_6 SAPS_9 SAPS_12 SAPS_18 SAPS_24;',
+      'MISSING=.;',
+      'USEVAR = SAPS_0-SAPS_24;',
+      'CLASSES = c(2);'),
+    MODEL = c(
+      '%OVERALL%',
+      'i s q cub | SAPS_0@0 SAPS_1@1 SAPS_2@2 SAPS_3@3 SAPS_6@6 
+                   SAPS_9@9 SAPS_12@12 SAPS_18@18 SAPS_24@24;',
+      glue('{g};'),
+      'SAPS_0 (1)
+      SAPS_1 (2)
+      SAPS_2 (3)
+      SAPS_3 (4)
+      SAPS_6 (5)
+      SAPS_9 (6)
+      SAPS_12 (7)
+      SAPS_18 (8)
+      SAPS_24 (9);'),
+    ANALYSIS = c(
+      'TYPE = MIXTURE;',
+      'STARTS = 1000 250;',
+      'PROCESSORS = 16;'),
+    OUTPUT = 'SAMPSTAT;',
+    SAVEDATA = glue('file = GMM3_{g}_res.dat;'),
+    autov = FALSE,
+    rdata = SAPS_df 
+  )
+}
+
+# Create, run, and read Mplus models 
+FitIndices_gmm3 <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  FitIndices_gmm3[[g]] <- mplusModeler(
+    object = gmm3_models[[g]],
+    dataout = glue(getwd(),'/SAPS/Results/GMM/GMM3/GMM3_{g}.dat'),
+    modelout = glue(getwd(),'/SAPS/Results/GMM/GMM3/GMM3_{g}.inp'),
+    hashfilename = FALSE,
+    run = 1,
+    check=TRUE,
+    varwarnings = TRUE,
+    writeData ="always"
+  )
+}
+
+  # GMM4 - free across t and k 
+gmm4_models <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+  gmm4_models[[g]] <- mplusObject(
+TITLE = glue('GMM4_{g};'),
+VARIABLE = c(
+  'NAMES = pin SAPS_0 SAPS_1 SAPS_2 SAPS_3 SAPS_6 SAPS_9 SAPS_12 SAPS_18 SAPS_24;',
+  'MISSING=.;',
+  'USEVAR = SAPS_0-SAPS_24;',
+  'CLASSES = c(2);'),
+MODEL = c(
+  '%OVERALL%',
+  'i s q cub | SAPS_0@0 SAPS_1@1 SAPS_2@2 SAPS_3@3 SAPS_6@6 
+               SAPS_9@9 SAPS_12@12 SAPS_18@18 SAPS_24@24;',
+  glue('{g};'),
+  
+  '%c#1%
+  SAPS_0 (1)
+  SAPS_1 (2)
+  SAPS_2 (3)
+  SAPS_3 (4)
+  SAPS_6 (5)
+  SAPS_9 (6)
+  SAPS_12 (7)
+  SAPS_18 (8)
+  SAPS_24 (9);',
+  
+  '%c#2%
+  SAPS_0 (10)
+  SAPS_1 (11)
+  SAPS_2 (12)
+  SAPS_3 (13)
+  SAPS_6 (14)
+  SAPS_9 (15)
+  SAPS_12 (16)
+  SAPS_18 (17)
+  SAPS_24 (18);'),
+ANALYSIS = c(
+  'TYPE = MIXTURE;',
+  'STARTS = 1000 250;',
+  'PROCESSORS = 16;'),
+OUTPUT = 'SAMPSTAT;',
+SAVEDATA = glue('file = GMM4_{g}_res.dat;'),
+autov = FALSE,
+rdata = SAPS_df 
+  )
+}
+
+# Create, run, and read Mplus models
+FitIndices_gmm4 <- list()
+for (g in c('i s-cub@0', 'i s q-cub@0', 'i s q cub@0', 'i s q cub')) {
+    FitIndices_gmm4 <- mplusModeler(
+    object = gmm4_models[[g]],
+    dataout = glue(getwd(),'/SAPS/Results/GMM/GMM4/GMM4_{g}.dat'),
+    modelout = glue(getwd(),'/SAPS/Results/GMM/GMM4/GMM4_{g}.inp'),
+    hashfilename = FALSE,
+    run = 1,
+    check=TRUE,
+    varwarnings = TRUE,
+    writeData ="always"
+  )
+}
+  
+### Step 7: Using individually varying timescores 
+  # Exclude cases with complete SAPS and missing date of observation 
+TSCORES_df <- TSCORES_df %>%
+  filter(xor(!is.na(SAPS_1), is.na(t1))) %>%
+  filter(xor(!is.na(SAPS_2), is.na(t2))) %>%
+  filter(xor(!is.na(SAPS_3), is.na(t3))) %>%
+  filter(xor(!is.na(SAPS_6), is.na(t6))) %>%
+  filter(xor(!is.na(SAPS_9), is.na(t9))) %>%
+  filter(xor(!is.na(SAPS_12), is.na(t12))) %>%
+  filter(xor(!is.na(SAPS_18), is.na(t18))) %>%
   filter(xor(!is.na(SAPS_24), is.na(t24)))
 
 #map <- map2(x, y, ~filter(xor(!is.na(x), is.na(y)))) # Does not work
-
-# Should try to update input file from best model with update() 
-
-# Create an Mplus model object
-m <- mplusObject(
-  TITLE = 'RealTime;',
+  # Create an Mplus model object - Should try to update input file from best model with update() 
+gbtm_tscores_models <- list()
+for (k in 1:3) {
+gbtm_tscores_models[[k]] <- mplusObject(
+  TITLE = glue('GBTM_K{k}_TSCORES;'),
   VARIABLE = c(
     'NAMES = pin SAPS_0 SAPS_1 SAPS_2 SAPS_3 SAPS_6 SAPS_9 SAPS_12 SAPS_18 SAPS_24
              t0 t1 t2 t3 t6 t9 t12 t18 t24;',
     'MISSING=.;',
     'USEVAR = SAPS_0-SAPS_24 t0-t24;',
     'TSCORES = t0-t24;', 
-    'CLASSES = c(2);'),
+    glue('CLASSES = c({k});')),
   MODEL = c(
     '%OVERALL%',
     'i s q cub | SAPS_0-SAPS_24 AT t0-t24;',
@@ -192,36 +436,57 @@ m <- mplusObject(
     'SAPS_0-SAPS_24 (1);'),
   ANALYSIS = c(
     'TYPE = MIXTURE RANDOM;',
-    'STARTS = 500 125;',
+    'STARTS = 1000 250;',
     'PROCESSORS = 16;'),
   OUTPUT = 'SAMPSTAT;',
+  SAVEDATA = glue('file = K{k}_TSCORES_res.dat;'),
   autov = FALSE,
   rdata = SAPS_df 
-)
-
-#Separate steps
-# Create, run, and read Mplus models
-m_fit <- mplusModeler(
-  object = m,
-  dataout = str_c(getwd(),'/MplusfromR/GBTM.dat'),
-  modelout = str_c(getwd(),'/MplusfromR/GBTM.inp'),
+)}
+  # Create, run, and read Mplus models 
+FitIndices_gbtm_tscores <- list()
+for (k in 1:3) {
+  
+FitIndices_gbtm_tscores <- mplusModeler(
+  object = gbtm_tscores_models[[k]],
+  dataout = glue(getwd(),'/SAPS/Results/GBTM/GBTM_K{k}_TSCORES.dat'),
+  modelout = glue(getwd(),'/SAPS/Results/GBTM/GBTM_K{k}_TSCORES.inp'),
   hashfilename = FALSE,
   run = 1,
   check=TRUE,
   varwarnings = TRUE,
   writeData ="always"
 )
+}
 
-# Create tab-delimited file and Mplus input syntax from R data.frame
+# --------------- #
+# MplusAutomation #
+# --------------- #
+
+### Prepare dataset
+  # remove characters from variables names > 8 characters 
+names(PEPP2_df) <- str_sub(names(PEPP2_df), 1, 8) 
+namesx2(PEPP2_df) # Function to check if column names are unique 
+namesx2 <- function(df) { 
+  
+  length1 <- length(colnames(df))
+  length2 <- length(unique(colnames(df)))        
+  if (length1 - length2 > 0 ) {
+    print(paste("There are", length1 - length2, " duplicates", sep=" "))
+  }     
+}
+anyDuplicated(colnames(PEPP2_df)) # locate column of the first duplicate)
+data.frame(colnames(PEPP2_df))
+
+  # Create tab-delimited file and Mplus input syntax from R data.frame
 prepareMplusData(
-  SAPS_df,
-  filename = str_c(getwd(),'/MplusfromR/K2_S500.dat'),
-  dropCols = t,
+  SAPScov_df,
+  filename = str_c(getwd(),'/MplusfromR/cov.dat'),
   inpfile = FALSE,
   writeData = "always"
 )
 
-# Create the Mplus input text for an mplusObject
+  # Create the Mplus input text for an mplusObject 
 inp <- createSyntax(
   object = m, 
   filename = str_c(getwd(),'/MplusfromR/GBTM.inp'),
@@ -229,22 +494,46 @@ inp <- createSyntax(
   add = FALSE) %>% 
 writeLines(str_c(getwd(),'/MplusfromR/GBTM.inp'))
 
-# Run Mplus Models
-runModels('/Users/olivierpercie/Desktop/MplusLGM/MplusfromR/K2_S500.inp')
-
+  # Run Mplus Models
+runModels('/Users/olivierpercie/Desktop/MplusLGM/MplusfromR/cov.inp')
 
 ### Add covariates
+SAPScov_df <- PEPP2_df %>% 
+  subset(miss_SAPS <= 4 & n == 1) %>% 
+  select ('pin', all_of(SAPS), 'K_SAPS', 'FIQ') 
 
-SAPScov_df <- PEPP2_df %>% select ('pin', all_of(SAPS), 'FIQ')
+# simple/univariate regression then multiple/multivariate 
+# missing on pred/Xs:
+#   - MI: has to be 2 separate steps/inp. files (AUXILIARY = vars excluded from IMPUTE = data)
+#   - add variance to model in manual 3 steps method 
+
+# cont
+cov_model <- update(final_model,
+VARIABLE = ~'
+USEVAR = SAPS_0-SAPS_24 ageentry;
+CLASSES = c(2);
+AUXILIARY = (R3STEP) ageentry;',
+ANALYSIS = ~'
+TYPE = MIXTURE;
+PROCESSORS = 16;',
+OUTPUT = ~'
+SAMPSTAT
+STANDARDIZED
+TECH1;',
+usevariables = names(SAPScov_df),
+rdata = df
+)
+
+cov_fit <- mplusModeler(
+  object = cov_model,
+  dataout = str_c(getwd(),'/MplusfromR/cov.dat'),
+  modelout = str_c(getwd(),'/MplusfromR/cov.inp'),
+  hashfilename = FALSE,
+  run = 1,
+  check=TRUE,
+  varwarnings = TRUE,
+  writeData = 'never'
+)
 
 
-model_cov <- update(final_model,
-                    VARIABLE = ~ . + 'AUXILARY = (R3STEP) FIQ;',
-                    ANALYSIS = ~ . + 'PROCESSORS = 16;',
-                    rdata = SAPScov_df,
-                    )
-
-
-
-
-
+save.image(glue(getwd(), 'SAPS', 'SAPS_{today()}.RData', .sep = "/"))
