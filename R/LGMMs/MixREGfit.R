8981617aa1d56f1fe449efda649af5a741bc967c
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2023-01-23 18:33:20 -0500

    Update MixREGfit.R
    - Extract results from param > output
    - Replace loop with purrr functions
    - Add 95% CI
    - Catch error

--- R/LGMMs/MixREGfit.R
+++ R/LGMMs/MixREGfit.R
@@ -1,67 +1,46 @@
-MixREGfit <- function(list_mpobj,
-         cov){
+MixREGfit <- function(list_mpobj) {
   
-  # Create empty lists to store for loop outputs
-  coef <- list()
-  intercepts <- list()
-  ORs <- list()
-  warn_err <- list()
-  
-  for (i in cov) {
-    
-    k <-  list_mpobj %>%  purrr::pluck(i, "results", "summaries", "NLatentClasses")
-    output <- pluck(list_mpobj, i, 'results', 'output')
-    
-    start <-  output %>% 
-      str_which("Latent Class 1") %>% 
-      first()
-    
-    end <-  output %>% 
-      str_which("QUALITY OF NUMERICAL RESULTS") %>% 
-      first()
-    
-    # Get Mplus warnings and errors 
-    warning <- pluck(list_mpobj, i, 'results', 'warnings')
-    error <- pluck(list_mpobj, i, 'results', 'errors')
-    
-    warn_err[[i]] <-
-      data.frame(
-        errors = ifelse(!is.null(error), paste(error), NA),
-        warnings = ifelse(is.null(warning), paste(warning), NA)
-      ) %>%
-      mutate('cov' = str_to_upper(i))
-    
-    # return NULL if Mplus warning or errors
-    if (is.null(error) == FALSE | is.null(warning) == FALSE) {
+      stdyx <- list_mpobj %>%
+        map_dfr(pluck, "results", "parameters", "stdyx.standardized", .default = NULL)
       
-      coef[[i]] <- NULL
+      ci <- list_mpobj %>% 
+        map_dfr(pluck, "results", "parameters", "ci.stdyx.standardized", .default = NULL)
       
-    } else {
+      stdyx_ci <- merge(stdyx, ci, all = TRUE) %>%
+        tryCatch(
+          expr = filter(., str_detect(paramHeader, "[:alpha:].ON")) %>%
+            select(., !matches("^(low|up)\\.?5")),
+          error = function(e)
+            .
+        ) 
+        
+      # mutate(ORs = case_when(!is.na(estimate) ~ exp(as.numeric(estimate)))) %>%  #compute OR and 95%CI
+    
+      warnings <- list_mpobj %>% 
+        map(pluck, "results", "warnings") %>% 
+        modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
+        map_dfc(pluck) %>% 
+        pivot_longer(cols = everything(), names_to = "param", values_to = 'warnings')  
       
-      output_ref <- output[start:end]
-      
-      line1 <-  output_ref %>% str_which("S\\s+ON") #get lines of regression coefs
-      
-      # Get coef
-      coef[[i]] <-
-        output_ref[line1 + 1] %>% 
-        str_split('[:space:]+', simplify = TRUE) %>%
-        as.data.frame() %>%
-        setNames(c('class', 'cov', 'estimate', 'se', 'tval', 'pval')) %>%
-        mutate(GF_S = str_c("C#", 1:(k))) %>% 
-        mutate(ORs = case_when(!is.na(estimate) ~ exp(as.numeric(estimate)))) %>%  #compute OR and 95%CI
-        mutate(LL_CI = exp(as.numeric(estimate) - 1.96 * as.numeric(se))) %>% 
-        mutate(UL_CI = exp(as.numeric(estimate) + 1.96 * as.numeric(se))) %>% 
-        mutate(across(c(ORs, LL_CI, UL_CI), round, 2))
-    }
-  }
+      errors <- list_mpobj %>% 
+        map(pluck, "results", "errors") %>% 
+        modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
+        map_dfc(pluck) %>% 
+        pivot_longer(cols = everything(), names_to = "param", values_to = 'errors')
   
   # Merge each data frame into one table
-  table <- list(coef, warn_err) %>%
-    map(reduce, merge, all = TRUE) %>%
-    reduce(merge, all = TRUE) %>%
-    select('cov', everything()) %>%
-    arrange(cov)
-  
+      table <- list(stdyx_ci, warnings, errors) %>%
+        reduce(merge, all = TRUE) %>%
+        tryCatch(
+          expr = 
+            select(., paramHeader, param, LatentClass, everything()) %>%
+            arrange(., param, LatentClass)%>% 
+            mutate(sig = case_when(pval < 0.001 ~ "***",
+                                   pval < 0.01 ~ "**",
+                                   pval < 0.05 ~ "*")),
+          error = function(e)
+            .
+        )
+             
   return(table)
 }
