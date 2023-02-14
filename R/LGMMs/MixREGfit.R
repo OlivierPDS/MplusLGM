924ec44dfdc9c21e355d96e7779e16a658283738
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2023-02-14 11:07:46 -0500

    Add std argument, and package names

--- R/LGMMs/MixREGfit.R
+++ R/LGMMs/MixREGfit.R
@@ -1,50 +1,66 @@
-MixREGfit <- function(list_mpobj) {
-  
-      stdyx <- list_mpobj %>%
-        map_dfr(pluck, "results", "parameters", "stdyx.standardized", .default = NULL)
-      
-      ci <- list_mpobj %>% 
-        map_dfr(pluck, "results", "parameters", "ci.stdyx.standardized", .default = NULL)
-      
-      stdyx_ci <- merge(stdyx, ci, all = TRUE) %>%
-        tryCatch(
-          expr = filter(., str_detect(paramHeader, ".+ON")) %>%
-            select(., !matches("^(low|up)\\.?5")),
-          error = function(e)
-            .
-        ) 
+MixREGfit <- function(list_mpobj, std = "unstd") {
+
+# Test arguments ----------------------------------------------------------
+# list_mpobj <- MixREG_models
+# std <- "stdyx"
+
+# Validate arguments & define values   ------------------------------------
+stopifnot(std %in% c("unstd", "stdyx", "stdy", "std"))
+
+std <- switch(std,
+              "unstd" = "unstandardized",
+              "stdyx" = "stdyx.standardized", 
+              "stdy" = "stdy.standardized",
+              "std" = "std.standardized")
+
+# Extract parameters and confidence intervals -----------------------------
+param <- list_mpobj %>%
+  purrr::map_dfr(pluck, "results", "parameters", std, .default = NULL)
+
+ci <- list_mpobj %>% 
+  purrr::map_dfr(pluck, "results", "parameters", glue::glue("ci.{std}"), .default = NULL) 
+
+param_ci <- merge(param, ci, all = TRUE) %>%
+  tryCatch(
+    expr = dplyr::filter(., stringr::str_detect(paramHeader, ".+ON")) %>%
+      dplyr::select(., !matches("^(low|up)\\.?5")),
+    error = function(e)
+      .
+  ) 
         
-      # mutate(ORs = case_when(!is.na(estimate) ~ exp(as.numeric(estimate)))) %>%  #compute OR and 95%CI
-    
-      warnings <- list_mpobj %>% 
-        map(pluck, "results", "warnings") %>% 
-        modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-        map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-        map(~ paste(.x, collapse = " ")) %>% 
-        map_dfc(pluck) %>% 
-        pivot_longer(cols = everything(), names_to = "param", values_to = 'warnings')  
-      
-      errors <- list_mpobj %>% 
-        map(pluck, "results", "errors") %>% 
-        modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-        map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-        map(~ paste(.x, collapse = " ")) %>% 
-        map_dfc(pluck) %>% 
-        pivot_longer(cols = everything(), names_to = "param", values_to = 'errors')
-  
-  # Merge each data frame into one table
-      table <- list(stdyx_ci, warnings, errors) %>%
-        reduce(merge, all = TRUE) %>%
-        tryCatch(
-          expr = 
-            select(., paramHeader, param, LatentClass, everything()) %>%
-            arrange(., param, LatentClass)%>% 
-            mutate(sig = case_when(pval < 0.001 ~ "***",
-                                   pval < 0.01 ~ "**",
-                                   pval < 0.05 ~ "*")),
-          error = function(e)
-            .
-        )
-             
-  return(table)
+# mutate(ORs = case_when(!is.na(estimate) ~ exp(as.numeric(estimate)))) %>%  #compute OR and 95%CI
+
+# Extract warnings and errors ---------------------------------------------
+warnings <- list_mpobj %>% 
+  purrr::map(pluck, "results", "warnings") %>% 
+  purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
+  purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
+  purrr::map(~ paste(.x, collapse = " ")) %>% 
+  purrr::map_dfc(pluck) %>% 
+  tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = 'warnings')  
+
+errors <- list_mpobj %>% 
+  purrr::map(pluck, "results", "errors") %>% 
+  purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
+  purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
+  purrr::map(~ paste(.x, collapse = " ")) %>% 
+  purrr::map_dfc(pluck) %>% 
+  tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = 'errors')
+
+
+# Merge each data frame into one table ------------------------------------
+table <- list(param_ci, warnings, errors) %>%
+  purrr::reduce(merge, all = TRUE) %>%
+  tryCatch(
+    expr = 
+      dplyr::select(., paramHeader, param, LatentClass, dplyr::everything()) %>%
+      dplyr::arrange(., param, LatentClass)%>% 
+      dplyr::mutate(sig = dplyr::case_when(pval < 0.001 ~ "***",
+                             pval < 0.01 ~ "**",
+                             pval < 0.05 ~ "*")),
+    error = function(e)
+      .
+    )
+         
+return(table)
 }
--- R/LGMMs/R3STEPfit.R
+++ R/LGMMs/R3STEPfit.R
@@ -1,88 +1,102 @@
-R3STEPfit <- function(list_mpobj) {
+R3STEPfit <- function(list_mpobj, std = "unstd") {
   
-  unstd <- list_mpobj %>%
-    map(pluck, "results", "parameters", "unstandardized", .default = NULL) %>% 
-    map2_dfr(., names(list_mpobj), ~ mutate(.x, name = .y)) %>% 
-    mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, name), paramHeader), .keep = "unused")
+# Test arguments ----------------------------------------------------------
+  # list_mpobj <- R3STEP_models
+  # std <- "stdyx"
+
+
+# Validate arguments & define values   ------------------------------------
+  stopifnot(std %in% c("unstd", "stdyx", "stdy", "std"))
+  
+  std <- switch(std,
+                "unstd" = "unstandardized",
+                "stdyx" = "stdyx.standardized", 
+                "stdy" = "stdy.standardized",
+                "std" = "std.standardized")
+  
+# Extract parameters and confidence intervals -----------------------------
+  param <- list_mpobj %>%
+    purrr::map(pluck, "results", "parameters", std, .default = NULL) %>% 
+    purrr::map2_dfr(., names(list_mpobj), ~ dplyr::mutate(.x, name = .y)) %>% 
+    dplyr::mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, name), paramHeader), .keep = "unused")
   
   ci <- list_mpobj %>% 
-    map(pluck, "results", "parameters", "ci.unstandardized", .default = NULL) %>% 
-    map2_dfr(., names(list_mpobj), ~ mutate(.x, name = .y)) %>% 
-    mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, name), paramHeader), .keep = "unused")
+    purrr::map(pluck, "results", "parameters", glue::glue("ci.{std}"), .default = NULL) %>% 
+    purrr::map2_dfr(., names(list_mpobj), ~ dplyr::mutate(.x, name = .y)) %>% 
+    dplyr::mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, name), paramHeader), .keep = "unused")
   
-  unstd_ci <- merge(unstd, ci, all = TRUE) %>%
+  param_ci <- merge(param, ci, all = TRUE) %>%
     tryCatch(
       expr = filter(., str_detect(LatentClass, "Categorical.Latent.Variables")) %>%
-        select(., !matches("^(low|up)\\.?5")),
+        dplyr::select(., !matches("^(low|up)\\.?5")),
       error = function(e)
         .
     ) 
-  
+
+# Extract warnings and errors ---------------------------------------------
   warnings <- list_mpobj %>% 
-    map(pluck, "results", "warnings") %>% 
-    modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-    map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-    map(~ paste(.x, collapse = " ")) %>% 
-    map_dfc(pluck) %>% 
-    pivot_longer(cols = everything(), names_to = "param", values_to = 'warnings') %>% 
-    mutate(param = str_to_upper(param))
+    purrr::map(pluck, "results", "warnings") %>% 
+    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
+    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
+    purrr::map(~ paste(.x, collapse = " ")) %>% 
+    purrr::map_dfc(pluck) %>% 
+    tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = 'warnings') %>% 
+    dplyr::mutate(param = stringr::str_to_upper(param))
   
   errors <- list_mpobj %>% 
-    map(pluck, "results", "errors") %>% 
-    modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-    map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-    map(~ paste(.x, collapse = " ")) %>% 
-    map_dfc(pluck) %>% 
-    pivot_longer(cols = everything(), names_to = "param", values_to = 'errors') %>% 
-    mutate(param = str_to_upper(param))
-  
-  
-  #     line1_alt <-  output_alt %>% str_which("C#\\d\\s+ON") #get lines of regression coefs and ORs
-  #     line2_alt <-  output_alt %>% str_which("Intercepts") #get lines of regression coefs and ORs
-  #     
+    purrr::map(pluck, "results", "errors") %>% 
+    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
+    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
+    purrr::map(~ paste(.x, collapse = " ")) %>% 
+    purrr::map_dfc(pluck) %>% 
+    tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = 'errors') %>% 
+    dplyr::mutate(param = stringr::str_to_upper(param))
+
+# Alternative parametrization ---------------------------------------------
+  #     line1_alt <-  output_alt %>% stringr::str_which("C#\\d\\s+ON") #get lines of regression coefs and ORs
+  #     line2_alt <-  output_alt %>% stringr::str_which("Intercepts") #get lines of regression coefs and ORs
   #     
   #     coef_alt[[i]] <-
   #       output_alt[line1_alt[c(rep(TRUE, (k-1)*(k-1)), rep(FALSE, (k-1)*(k-1)))] + 1] %>% #Manual R3STEP
   #       as.data.frame() %>%
   #       setNames(c('class', 'cov', 'estimate', 'se', 'tval', 'pval')) %>%
-  #       str_split('[:space:]+', simplify = TRUE) %>%
-  #       mutate(class = str_c('C#', parse_number(output_alt[line_alt[c(rep(TRUE, (k-1)*(k-1)), rep(FALSE, (k-1)*(k-1)))]]))) %>% 
-  #       mutate(ref = str_c('Ref C#', rep(seq(k-1), each=k-1)))
+  #       stringr::str_split('[:space:]+', simplify = TRUE) %>%
+  #       dplyr::mutate(class = stringr::str_c('C#', parse_number(output_alt[line_alt[c(rep(TRUE, (k-1)*(k-1)), rep(FALSE, (k-1)*(k-1)))]]))) %>% 
+  #       dplyr::mutate(ref = stringr::str_c('Ref C#', rep(seq(k-1), each=k-1)))
   # 
   #     intercepts_alt[[i]] <-
   #       output_alt[sort(as.vector(outer(line2_alt, seq_along(line2_alt), '+')))] %>% #Manual R3STEP
-  #       str_split('[:space:]+', simplify = TRUE) %>%
+  #       stringr::str_split('[:space:]+', simplify = TRUE) %>%
   #       as.data.frame() %>%
-  #       select(-1) %>%
+  #       dplyr::select(-1) %>%
   #       setNames(c('class', 'estimate', 'se', 'tval', 'pval')) %>%
-  #       mutate(class = str_replace(class, 'C#', 'intercept')) %>% 
-  #       mutate(ref = str_c('Ref C#', rep(seq(k-1), each=k-1))) %>% 
-  #       mutate('cov' = str_to_upper(i))
+  #       dplyr::mutate(class = stringr::str_replace(class, 'C#', 'intercept')) %>% 
+  #       dplyr::mutate(ref = stringr::str_c('Ref C#', rep(seq(k-1), each=k-1))) %>% 
+  #       dplyr::mutate('cov' = stringr::str_to_upper(i))
   #     
   #     ORs_alt[[i]] <-
   #       output_alt[line1_alt[c(rep(FALSE, (k-1)*(k-1)), rep(TRUE, (k-1)*(k-1)))] + 1] %>% #Manual R3STEP
-  #       str_split('[:space:]+', simplify = TRUE) %>%
+  #       stringr::str_split('[:space:]+', simplify = TRUE) %>%
   #       as.data.frame() %>%
   #       setNames(c('class', 'cov', 'OR', 'OR_se', '[OR_CI', 'OR_CI]')) %>%
-  #       mutate(class = str_c('C#', parse_number(output_alt[line_alt[c(rep(TRUE, (k-1)*(k-1)), rep(FALSE, (k-1)*(k-1)))]]))) %>% 
-  #       mutate(ref = str_c('Ref C#', rep(seq(k-1), each=k-1)))
+  #       dplyr::mutate(class = stringr::str_c('C#', parse_number(output_alt[line_alt[c(rep(TRUE, (k-1)*(k-1)), rep(FALSE, (k-1)*(k-1)))]]))) %>% 
+  #       dplyr::mutate(ref = stringr::str_c('Ref C#', rep(seq(k-1), each=k-1)))
   #     
-  # }
-  
   # table_alt <- list(coef_alt, intercepts_alt, ORs_alt, warn_err) %>%
-  #   map(reduce, merge, all = TRUE) %>%
-  #   reduce(merge, all = TRUE) %>%
-  #   select('cov', everything()) %>%
-  #   arrange(ref, cov)
+  #   purrr::map(purrr::reduce, merge, all = TRUE) %>%
+  #   purrr::reduce(merge, all = TRUE) %>%
+  #   dplyr::select('cov', everything()) %>%
+  #   dplyr::arrange(ref, cov)
   
-  # Merge each data frame into one table
-  table <- list(unstd_ci, warnings, errors) %>%
-    reduce(merge, all = TRUE) %>%
+
+# Merge parameters, confidence intervals, warnings and errors into --------
+  table <- list(param_ci, warnings, errors) %>%
+    purrr::reduce(merge, all = TRUE) %>%
     tryCatch(
       expr = 
-        select(., paramHeader, param, everything(), -LatentClass) %>%
-        arrange(., paramHeader, param) %>% 
-        mutate(sig = case_when(pval < 0.001 ~ "***",
+        dplyr::select(., paramHeader, param, everything(), -LatentClass) %>%
+        dplyr::arrange(., paramHeader, param) %>% 
+        dplyr::mutate(sig = dplyr::case_when(pval < 0.001 ~ "***",
                                pval < 0.01 ~ "**",
                                pval < 0.05 ~ "*")),
       error = function(e)
