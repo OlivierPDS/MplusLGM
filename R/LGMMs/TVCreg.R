54d059bb43cd033cd0cafb56a3f229f6b21ab542
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2023-02-23 12:22:04 -0500

    Define cov when categorical

--- R/LGMMs/TVCreg.R
+++ R/LGMMs/TVCreg.R
@@ -16,7 +16,7 @@ TVCreg <- function(df,
   # df <-  SAPS_df
   # idvar  <-  'pin'
   # usevar <-  SAPS
-  # cov <-  list(SOFAS, SANS, CDS, HAS, YMRS)
+  # cov <-  list(SANS, CDS, PSR)
   # starts  <-  0
   # output  <-  c("SAMPSTAT", "CINTERVAL")
   # model  <-  FINAL_model
@@ -31,6 +31,14 @@ TVCreg <- function(df,
               \(cov) subset(cov, stringr::str_detect(cov, paste(parse_number(usevar), collapse = "|"))), 
               .else = \(cov) identity(cov))
   
+  cov_dummy <- cov %>%  
+    map_depth(1, #at
+    \(x) purrr::map_if(x,#x
+      \(x) is.factor(df[[x]]), #p
+      \(x) map_chr(1:(nlevels(df[[x]]) - 1), #f, y
+                   \(y) paste0(x, y)) %>% #f
+        paste(collapse = " ")))
+    
   usevar_lst <- cov %>% 
     purrr::map_if(\(x) length(usevar) > length(x), 
            \(x) subset(usevar, stringr::str_detect(usevar, paste(parse_number(x), collapse = "|"))), 
@@ -47,7 +55,7 @@ TVCreg <- function(df,
     purrr::map(cov,
         \(z) merge(
           x = model[["results"]][["savedata"]], 
-          y = dplyr::select(df, c(idvar, z)),
+          y = dplyr::select(df, all_of(c(idvar, z)), -any_of(usevar)),
           by.x = stringr::str_to_upper(idvar),
           by.y = idvar,
           all = TRUE
@@ -61,9 +69,9 @@ TVCreg <- function(df,
     purrr::map(~ MplusAutomation::parseMplus(.x, add = TRUE))
 
 ## VARIABLE = 
-  usevars <- cov %>% 
+  usevars <- cov_dummy %>% 
     purrr::map(\(x) paste(x, collapse = " ")) %>%
-    purrr::map2(usevar_col, \(x, y) paste("USEVAR =", x, y, "N", collapse = " "))
+    purrr::map2(usevar_col, \(x, y) paste("USEVAR =", "N", y, x, collapse = " "))
     
   nominal <- "NOMINAL = N"
   
@@ -74,7 +82,25 @@ TVCreg <- function(df,
     purrr::map(~ strwrap(.x, width = 90, exdent = 5)) %>%
     purrr::map(~ paste(.x, collapse = "\n")) %>%
     purrr::map(~ gsub(";", ";\n",.x))
-  
+
+## DEFINE =
+  define <- cov %>% 
+    purrr::map_depth(1, #at
+              \(x) purrr::map_if(x, #x
+                                 \(x) is.factor(df[[x]]), #p
+                                 \(x) purrr::map_chr(1:(nlevels(df[[x]]) - 1), #f, y
+                                                     \(y) glue::glue("{x}{y} = {x} == {y}")),
+                                 .else = ~ NULL )) %>% 
+    purrr::map_depth(1,
+              \(x) purrr::map_if(x,  
+                                 \(x) !is.null(x), 
+                                 \(x) MplusAutomation::parseMplus(x, add = TRUE))) %>% 
+    purrr::map_if(\(x) !all(purrr::map_lgl(x, \(y) is.null(y))), 
+                  \(x) paste(x, collapse = " "), 
+                  .else = ~ NULL) %>% 
+    purrr::map_if(\(x) !is.null(x),
+                  \(x) gsub(";", ";\n", x))
+
 ## ANALYSIS =
   type <- "TYPE = MIXTURE"
   start_val <-  glue::glue("STARTS = {starts}")
@@ -90,10 +116,10 @@ TVCreg <- function(df,
     split(rep(1:k, each = k)) %>%
     purrr::map(\(x) utils::head(x, -1))  #logits specification
 
-  reg <- purrr::map2(usevar_lst, cov, \(x, y) 
+  reg <- purrr::map2(usevar_lst, cov_dummy, \(x, y) 
               purrr::map2(x, y, \(x, y) glue::glue("{x} ON {y}"))) #regressions specification
 
-  variance <- purrr::map(cov, \(x) paste(x, collapse = " ")) #covariates variance specification
+  variance <- purrr::map(cov_dummy, \(x) paste(x, collapse = " ")) #covariates variance specification
 
 ### Model specification
   model1 <- list(reg, variance) %>%
@@ -117,10 +143,11 @@ TVCreg <- function(df,
   outputs <- MplusAutomation::parseMplus(output, add = TRUE)
   
 # Create Mplus object -----------------------------------------------------
-  mpobj <- purrr::pmap(list(title, variable, model5, savedata),  \(title, variable, model, savedata)
+  mpobj <- purrr::pmap(list(title, variable, define, model5, savedata),  \(title, variable, define, model, savedata)
                 MplusAutomation::mplusObject(
                   TITLE = title,
                   VARIABLE = variable,
+                  DEFINE = define,
                   MODEL = model,
                   ANALYSIS = analysis,
                   OUTPUT = outputs,
