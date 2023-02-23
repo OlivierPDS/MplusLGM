1a0e04e9c92f01ae31f3fb519120cc89fce97e65
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2023-02-23 12:18:25 -0500

    Add arguments for standardization, add probability scale,  improve final table

--- R/LGMMs/D3STEPfit.R
+++ R/LGMMs/D3STEPfit.R
@@ -1,82 +1,129 @@
-D3STEPfit <- function(list_mpobj, std = "std"){
- 
- # Validate arguments & define values   ------------------------------------
-stopifnot(std %in% c("unstd", "stdyx", "stdy", "std"))
-
-std <- switch(std,
-"unstd" = "unstandardized",
-"stdyx" = "stdyx.standardized", 
-"stdy" = "stdy.standardized",
-"std" = "std.standardized")
+D3STEPfit <- function(list_mpobj, std = "unstd") {
   
-# Extract parameters and confidence intervals -----------------------------
-param <- list_mpobj %>%
-   purrr::discard( ~ stringr::str_detect(.[["VARIABLE"]], "CATEGORICAL")) %>% 
-   purrr::map(pluck, "results", "parameters", std, .default = NULL) %>% 
-   purrr::map2_dfr(., names(.), ~ if(!is.null(.x)) {dplyr::mutate(.x, name = .y)}) %>% 
-   dplyr::mutate(param = ifelse(paramHeader == "New.Additional.Parameters", paste(name, param), param))
+  # Test arguments ----------------------------------------------------------
+  # list_mpobj <- D3STEP_models
+  # std <- "stdyx"
+  # ref <- 2
   
-ci <- list_mpobj %>%
-  purrr::discard( ~ stringr::str_detect(.[["VARIABLE"]], "CATEGORICAL")) %>% 
-  purrr::map(pluck, "results", "parameters", glue::glue("ci.{std}"), .default = NULL) %>% 
-  purrr::map2_dfr(., names(.), ~ if(!is.null(.x)) {dplyr::mutate(.x, name = .y)}) %>% 
-  dplyr::mutate(param = ifelse(paramHeader == "New.Additional.Parameters", paste(name, param), param))
+  # Validate arguments & define values   ------------------------------------
+  stopifnot(std %in% c("unstd", "stdyx", "stdy", "std"))
   
-param_ci <- merge(param, ci, all = TRUE) %>% 
-  tryCatch(
-    expr = 
-      dplyr::filter(., stringr::str_detect(param, "[:alpha:]#\\d", negate = TRUE)) %>%
-      dplyr::select(., !matches("^(low|up)\\.?5")),
-    error = function(e)
-      .
-  ) 
+  std <- switch(std,
+    "unstd" = "unstandardized",
+    "stdyx" = "stdyx.standardized",
+    "stdy" = "stdy.standardized",
+    "std" = "std.standardized"
+  )
   
-probs <- list_mpobj %>%
-  purrr::map_dfr(pluck, "results", "parameters", "probability.scale", .default = NULL) %>%
-  tryCatch(
-    expr = dplyr::mutate(., paramHeader = stringr::str_c("Category", category), .keep = "unused"),
-    error = function(e)
-      .
-  ) # return empty table if NULL
+  # Extract parameters and confidence intervals -----------------------------
+  param <- list_mpobj %>%
+    purrr::map(pluck, "results", "parameters", std, .default = NULL) %>%
+    purrr::map2_dfr(., names(.), 
+                    ~ if (!is.null(.x)) {dplyr::mutate(.x, name = str_to_upper(.y))}) %>%
+    dplyr::mutate(param = ifelse(
+      paramHeader == "New.Additional.Parameters",
+      paste(name, param),
+      param
+    ))
   
-wt <- list_mpobj %>% 
-  purrr::map_dfr(pluck, "results", "summaries", .default = NULL) %>% 
-  dplyr::select(starts_with("Wald")) %>% 
-  dplyr::mutate(param = stringr::str_to_upper(names(list_mpobj)))
-
-# Extract warnings and errors ---------------------------------------------
-warnings <- list_mpobj %>% 
-  purrr::map(pluck, "results", "warnings", .default = NULL) %>% 
-  purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-  purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-  purrr::map(~ paste(.x, collapse = " ")) %>% 
-  dplyr::bind_rows() %>% 
-  tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = "warnings") %>% 
-  dplyr::mutate(param = stringr::str_to_upper(param))
-
-errors <- list_mpobj %>% 
-  purrr::map(pluck, "results", "errors") %>% 
-  purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-  purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-  purrr::map(~ paste(.x, collapse = " ")) %>% 
-  dplyr::bind_rows() %>% 
-  tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = "errors") %>% 
-  dplyr::mutate(param = stringr::str_to_upper(param))
-
-# Merge each data frame into one table ------------------------------------
-table <- list(param_ci, probs, wt, warnings, errors) %>%
-  purrr::compact() %>% 
-  purrr::reduce(merge, all = TRUE) %>%
-  tryCatch(
-    expr = dplyr::select(., param, paramHeader, LatentClass, est, se, est_se, pval, low2.5, up2.5,
-                           WaldChiSq_Value, WaldChiSq_DF, WaldChiSq_PValue, warnings, errors) %>%
-      dplyr::arrange(., param, LatentClass) %>% 
-      dplyr::mutate(sig = dplyr::case_when(WaldChiSq_PValue < 0.001 ~ "***",
-                             WaldChiSq_PValue < 0.01 ~ "**",
-                             WaldChiSq_PValue < 0.05 ~ "*")),
-    error = function(e)
-      .
-  ) 
+  ci <- list_mpobj %>%
+    purrr::map(pluck,
+               "results",
+               "parameters",
+               glue::glue("ci.{std}"),
+               .default = NULL) %>%
+    purrr::map2_dfr(., names(.), ~ if (!is.null(.x)) {
+      dplyr::mutate(.x, name = str_to_upper(.y))
+    }) %>%
+    dplyr::mutate(param = ifelse(
+      paramHeader == "New.Additional.Parameters",
+      paste(name, param),
+      param
+    ))
   
-return(table)
+  param_ci <- merge(param, ci, all = TRUE)
+  
+  probs <- list_mpobj %>%
+    purrr::map(pluck,
+                   "results",
+                   "parameters",
+                   "probability.scale",
+                   .default = NULL) %>% 
+    purrr::map2_dfr(., names(.),
+                    ~ if (!is.null(.x)) {dplyr::mutate(.x, name = .y)}) %>%
+    tryCatch(
+      expr = 
+        dplyr::mutate(., 
+                      low2.5 = est - 1.96 * se,
+                      up2.5 = est + 1.96 * se) %>% 
+        dplyr::mutate(dplyr::across(c('low2.5', 'est', 'up2.5'), .names = 'PRs_{.col}'), .keep = "unused") %>% 
+        dplyr::mutate(param = paste0(param, "$", category), .keep = "unused") %>% 
+        dplyr::mutate(name = stringr::str_to_upper(name)) %>%
+        dplyr::select(-se, -est_se),
+      error = function(e)
+        .
+    ) # return empty table if NULL
+  
+  wt <- list_mpobj %>%
+    purrr::map_dfr(pluck, "results", "summaries", .default = NULL) %>%
+    dplyr::select(starts_with("Wald")) %>%
+    dplyr::mutate(name = stringr::str_to_upper(names(list_mpobj)))
+  
+  # Extract warnings and errors ---------------------------------------------
+  warnings <- list_mpobj %>%
+    purrr::map(pluck, "results", "warnings", .default = NULL) %>%
+    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>%
+    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>%
+    purrr::map(~ paste(.x, collapse = " ")) %>%
+    dplyr::bind_rows() %>%
+    tidyr::pivot_longer(cols = everything(),
+                        names_to = "name",
+                        values_to = "warnings") %>%
+    dplyr::mutate(name = stringr::str_to_upper(name))
+  
+  errors <- list_mpobj %>%
+    purrr::map(pluck, "results", "errors") %>%
+    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>%
+    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>%
+    purrr::map(~ paste(.x, collapse = " ")) %>%
+    dplyr::bind_rows() %>%
+    tidyr::pivot_longer(cols = everything(),
+                        names_to = "name",
+                        values_to = "errors") %>%
+    dplyr::mutate(name = stringr::str_to_upper(name))
+  
+  # Merge each data frame into one table ------------------------------------
+  table <- list(param_ci, probs, wt, warnings, errors) %>%
+    purrr::compact() %>%
+    purrr::reduce(merge, all = TRUE) %>%
+    tryCatch(
+      expr = 
+        dplyr::filter(.,
+          paramHeader == "Thresholds" |
+          paramHeader == "New.Additional.Parameters" |
+          (paramHeader == "Means" & stringr::str_detect(param, "[:alpha:]#\\d", negate = TRUE))) %>%
+        dplyr::select(
+          LatentClass,
+          param,
+          paramHeader,
+          est,
+          se,
+          pval,
+          dplyr::starts_with("Wald"),
+          dplyr::starts_with("PRs"),
+          warnings,
+          errors) %>%
+        dplyr::arrange(LatentClass, paramHeader, param) %>%
+        dplyr::mutate(
+          sig = dplyr::case_when(
+            WaldChiSq_PValue < 0.001 ~ "***",
+            WaldChiSq_PValue < 0.01 ~ "**",
+            WaldChiSq_PValue < 0.05 ~ "*"
+          )
+        ),
+      error = function(e)
+        .
+    )
+  
+  return(table)
 }
