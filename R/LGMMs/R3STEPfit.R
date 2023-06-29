7b72744f1a1817442adaee78810d18065b0934f7
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2023-06-29 15:16:36 -0400

    Add selection of alternative parameterization

--- R/LGMMs/R3STEPfit.R
+++ R/LGMMs/R3STEPfit.R
@@ -1,122 +1,80 @@
-R3STEPfit <- function(list_mpobj, 
-                      std = "unstd", 
-                      ref = 1) {
-  
-# Test arguments ----------------------------------------------------------
+R3STEPfit <- function(list_mpobj,
+                      std = "unstd",
+                      ref = NULL) {
+  # Test arguments ----------------------------------------------------------
   # list_mpobj <- R3STEP_models
   # std <- "stdyx"
-  # ref <- 2
+  # ref <- 1
 
-
-# Validate arguments & define values   ------------------------------------
+  # Validate arguments & define values   ------------------------------------
   stopifnot(std %in% c("unstd", "stdyx", "stdy", "std"))
-  
-  std <- switch(std,
-                "unstd" = "unstandardized",
-                "stdyx" = "stdyx.standardized", 
-                "stdy" = "stdy.standardized",
-                "std" = "std.standardized")
-  
-  stopifnot(ref %in% c("1", "2"))
-  
-# Extract parameters and confidence intervals -----------------------------
-  param <- list_mpobj %>%
-    purrr::map(pluck, "results", "parameters", std, .default = NULL) %>% 
-    purrr::map2_dfr(., names(list_mpobj), \(x, y) if(!is.null(x)){dplyr::mutate(x, name = stringr::str_to_upper(y))}) %>% 
-    dplyr::mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, param), paramHeader)) %>%
-    dplyr::mutate(param = ifelse(str_detect(paramHeader, "^Intercepts"), name, param)) %>% 
-    dplyr::mutate(param = ifelse(str_detect(param, "N#\\d"), paste(name, param), param))
-  
-  ci <- list_mpobj %>% 
-    purrr::map(pluck, "results", "parameters", glue::glue("ci.{std}"), .default = NULL) %>% 
-    purrr::map2_dfr(., names(list_mpobj), \(x, y) if(!is.null(x)){dplyr::mutate(x, name = stringr::str_to_upper(y))}) %>% 
-    dplyr::mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, param), paramHeader)) %>%
-    dplyr::mutate(param = ifelse(str_detect(paramHeader, "^Intercepts|^N#\\d"), name, param)) %>% 
-    dplyr::mutate(param = ifelse(str_detect(param, "N#\\d"), paste(name, param), param))
-  
-  param_ci <- merge(param, ci, all = TRUE)
 
-# Extract warnings and errors ---------------------------------------------
-  warnings <- list_mpobj %>% 
-    purrr::map(pluck, "results", "warnings") %>% 
-    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-    purrr::map(~ paste(.x, collapse = " ")) %>% 
-    purrr::map_dfc(pluck) %>% 
-    tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = 'warnings') %>% 
-    dplyr::mutate(param = stringr::str_to_upper(param))
-  
-  errors <- list_mpobj %>% 
-    purrr::map(pluck, "results", "errors") %>% 
-    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>% 
-    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>% 
-    purrr::map(~ paste(.x, collapse = " ")) %>% 
-    purrr::map_dfc(pluck) %>% 
-    tidyr::pivot_longer(cols = everything(), names_to = "param", values_to = 'errors') %>% 
-    dplyr::mutate(param = stringr::str_to_upper(param))
-    
-# Merge parameters, confidence intervals, warnings and errors  ------------
-  table <- list(param_ci, warnings, errors) %>%
-    purrr::reduce(merge, all = TRUE)
-  
-# Alternative parameterization using reference class 1 or 2 --------------------
-  
-  if (ref == 1) {
-    table <- table %>%
-      tryCatch(
-        expr =
-          dplyr::mutate(., 
-                        dplyr::across(c('low2.5', 'est', 'up2.5'), 
-                                      ~ ifelse(str_detect(paramHeader, "C#\\d.ON"), 
-                                               round(exp(.x), 2), 
-                                               NA), 
-                                      .names = 'ORs_{.col}'
-          )),
-        error = function(e)
-          .
-      )
-  } else if (ref == 2) {
-    table <- table %>%
-      tryCatch(
-        expr =
-          dplyr::mutate(., 
-                        dplyr::across(c('low2.5', 'est', 'up2.5'), 
-                                      ~ ifelse(str_detect(paramHeader, "C#\\d.ON"), 
-                                               round(exp(-.x), 2), 
-                                               NA), 
-                                      .names = 'ORs_{.col}'
-                        )) %>%
-          mutate(
-            paramHeader = str_replace(paramHeader, "1", "2"),
-            est = -est,
-            ORs_low2.5 = ORs_up2.5,
-            ORs_up2.5 = .$ORs_low2.5
-          ),
-        error = function(e)
-          .
-      )
+  std <- switch(std,
+    "unstd" = "unstandardized",
+    "stdyx" = "stdyx.standardized",
+    "stdy" = "stdy.standardized",
+    "std" = "std.standardized"
+  )
+
+  if (is.null(ref)) {
+    ref <- purrr::pluck(list_mpobj, 1, "results", "summaries", "NLatentClasses")
   }
-  
-# Final table --------------------------------------------------------
-  table <- table %>%
-    tryCatch(
-      expr = 
-        filter(., if (std == "unstandardized") {
-          stringr::str_detect(LatentClass, "Categorical.Latent.Variables")
-        } else{
-          paramHeader == "Means" |
-            stringr::str_detect(param, "N#\\d")
-        }) %>%  # filter out irrelevant parameters
-        dplyr::mutate(sig = dplyr::case_when(pval < 0.001 ~ "***",
-                                             pval < 0.01 ~ "**",
-                                             pval < 0.05 ~ "*")) %>%  #add significativity
-        dplyr::mutate(dplyr::across(c(tidyselect::where(is.numeric), -pval), ~ round(.x, digits = 2))) %>%
-        dplyr::select(paramHeader, param, est, se, pval, sig, dplyr::starts_with("ORs"), warnings, errors) %>%
-        dplyr::arrange(paramHeader, -est, param),
-      error = function(e)
-        .
-    )
-  
+  stopifnot(ref %in% seq(ref))
+
+  # Extract warnings and errors ---------------------------------------------
+  warnings <- list_mpobj %>%
+    purrr::map(pluck, "results", "warnings", .default = NULL)
+
+  errors <- list_mpobj %>%
+    purrr::map(pluck, "results", "errors", .default = NULL)
+
+  warn_err <- purrr::map2(warnings, errors, \(x, y) dplyr::tibble(warnings = paste(x, collapse = " "), errors = paste(y, collapse = " "))) %>%
+    purrr::map(~ mutate(.x, dplyr::across(tidyselect::everything(), ~ stringr::str_remove_all(.x, "[[:punct:]]")))) %>%
+    purrr::map(~ mutate(.x, dplyr::across(tidyselect::everything(), ~ dplyr::na_if(.x, "")))) %>%
+    purrr::imap(\(x, idx) dplyr::mutate(x, name = stringr::str_to_upper(idx))) %>%
+    purrr::reduce(merge, all = TRUE)
+
+  # Extract parameters or alternative parameterization ------------------------------------------------------
+  sublist_mpobj <- list_mpobj %>%
+    purrr::discard(purrr::map_vec(errors, ~ length(.x) > 0))
+
+  if (ref == last(seq(ref))) {
+    param <- purrr::map(sublist_mpobj, ~ purrr::pluck(.x, "results", "parameters", std, .default = NULL))
+  } else {
+    param <- purrr::map(sublist_mpobj, ~ purrr::pluck(.x, "results", "parameters", "unstandardized.alt", glue::glue("ref.cat.{ref}"), .default = NULL))
+  }
+
+  param <- param %>%
+    purrr::imap(\(x, idx) dplyr::mutate(x, name = stringr::str_to_upper(idx))) %>%
+    purrr::reduce(merge, all = TRUE) %>% # Error with bind_rows and list_rbind: "Can't combine `est_se` <double> and `est_se` <character>."
+    dplyr::mutate(paramHeader = ifelse(paramHeader == "Intercepts", paste(paramHeader, param), paramHeader)) %>%
+    dplyr::mutate(param = ifelse(stringr::str_detect(paramHeader, "^Intercepts"), name, param)) %>%
+    dplyr::mutate(param = ifelse(stringr::str_detect(param, "N#\\d"), paste(name, param), param))
+
+  # Compute odd ratio and confidence intervals -----------------------------
+  or_ci <- param %>%
+    dplyr::mutate(low2.5 = est - 1.96 * se, up2.5 = est + 1.96 * se) %>%
+    dplyr::mutate(dplyr::across(c("est", "low2.5", "up2.5"),
+      ~ ifelse(str_detect(paramHeader, "C#\\d.ON"),
+        exp(.x),
+        NA
+      ),
+      .names = "OR_{.col}"
+    ))
+
+  # Merge parameters, confidence intervals, warnings and errors  ------------
+  table <- list(or_ci, warn_err) %>%
+    purrr::reduce(full_join, by = "name") %>%
+    dplyr::mutate(param = dplyr::coalesce(param, name)) %>%
+    dplyr::filter(stringr::str_detect(paramHeader, "C#\\d") | !is.na(errors)) %>% # filter out irrelevant parameters
+    dplyr::mutate(sig = dplyr::case_when(
+      pval < 0.001 ~ "***",
+      pval < 0.01 ~ "**",
+      pval < 0.05 ~ "*"
+    )) %>% # add significativity
+    dplyr::mutate(dplyr::across(c(tidyselect::where(is.numeric), -pval), ~ round(.x, digits = 2))) %>%
+    dplyr::select(paramHeader, param, est, se, pval, sig, tidyselect::starts_with("OR"), warnings, errors) %>%
+    dplyr::arrange(paramHeader, -est, param)
+
   return(table)
-  
 }
