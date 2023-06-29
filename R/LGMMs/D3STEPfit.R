d8f80de3305864b95f3bc83497b1d9ecca675213
OlivierPDS <80708163+OlivierPDS@users.noreply.github.com>
2023-06-29 15:08:47 -0400

    Add SMD, OR computation

--- R/LGMMs/D3STEPfit.R
+++ R/LGMMs/D3STEPfit.R
@@ -1,92 +1,107 @@
 D3STEPfit <- function(list_mpobj, std = "unstd") {
-  
   # Test arguments ----------------------------------------------------------
   # list_mpobj <- D3STEP_models
   # std <- "stdyx"
-  # ref <- 2
-  
+
   # Validate arguments & define values   ------------------------------------
   stopifnot(std %in% c("unstd", "stdyx", "stdy", "std"))
-  
+
   std <- switch(std,
     "unstd" = "unstandardized",
     "stdyx" = "stdyx.standardized",
     "stdy" = "stdy.standardized",
     "std" = "std.standardized"
   )
-  
-  # Extract parameters and confidence intervals -----------------------------
-  param <- list_mpobj %>%
-    purrr::map(pluck, "results", "parameters", std, .default = NULL) %>%
-    purrr::map2_dfr(., names(.), ~ if (!is.null(.x)) {dplyr::mutate(.x, name = stringr::str_to_upper(.y))}) %>%
-    dplyr::mutate(param = ifelse(paramHeader == "New.Additional.Parameters", paste(name, param), param))
-  
-  ci <- list_mpobj %>% 
-    purrr::map(pluck, "results", "parameters", glue::glue("ci.{std}"), .default = NULL) %>%
-    purrr::map2_dfr(., names(.), ~ if (!is.null(.x)) {dplyr::mutate(.x, name = stringr::str_to_upper(.y))}) %>%
-    dplyr::mutate(param = ifelse(paramHeader == "New.Additional.Parameters", paste(name, param),param))
-  
-  param_ci <- merge(param, ci, all = TRUE)
 
-  # Extract/compute probability scale and Wald test ---------------------------------
-   probs <- list_mpobj %>%
-    purrr::map(pluck, "results", "parameters", "probability.scale", .default = NULL) %>% 
-    purrr::map2_dfr(., names(.), ~ if (!is.null(.x)) {dplyr::mutate(.x, name = .y)}) %>%
-    tryCatch(
-      expr = 
-        dplyr::mutate(., low2.5 = est - 1.96 * se, up2.5 = est + 1.96 * se) %>%
-        dplyr::mutate(dplyr::across(c('est', 'pval', 'low2.5', 'up2.5'), .names = 'PRs_{.col}'), .keep = "unused") %>% 
-        dplyr::mutate(param = paste0(param, "$", category), .keep = "unused") %>% 
-        dplyr::mutate(name = stringr::str_to_upper(name)) %>%
-        dplyr::select(-se, -est_se),
-      error = function(e)
-        .
-    ) # return empty table if NULL
-  
-  wt <- list_mpobj %>%
-    purrr::map_dfr(pluck, "results", "summaries", .default = NULL) %>%
-    dplyr::select(dplyr::starts_with("Wald")) %>%
-    dplyr::mutate(name = stringr::str_to_upper(names(list_mpobj)))
-  
   # Extract warnings and errors ---------------------------------------------
   warnings <- list_mpobj %>%
-    purrr::map(pluck, "results", "warnings", .default = NULL) %>%
-    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>%
-    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>%
-    purrr::map(~ paste(.x, collapse = " ")) %>%
-    dplyr::bind_rows() %>%
-    tidyr::pivot_longer(cols = everything(), names_to = "name", values_to = "warnings") %>%
-    dplyr::mutate(name = stringr::str_to_upper(name))
-  
+    purrr::map(pluck, "results", "warnings", .default = NULL)
+
   errors <- list_mpobj %>%
-    purrr::map(pluck, "results", "errors") %>%
-    purrr::modify_if(~ length(.x) > 0, paste, .else = ~ NA) %>%
-    purrr::map(~ gsub("[^[:alnum:][:space:]]", "", .x)) %>%
-    purrr::map(~ paste(.x, collapse = " ")) %>%
-    dplyr::bind_rows() %>%
-    tidyr::pivot_longer(cols = everything(), names_to = "name", values_to = "errors") %>%
-    dplyr::mutate(name = stringr::str_to_upper(name))
+    purrr::map(pluck, "results", "errors", .default = NULL)
+
+  warn_err <- purrr::map2(warnings, errors, \(x, y) dplyr::tibble(warnings = paste(x, collapse = " "), errors = paste(y, collapse = " "))) %>%
+    purrr::map(~ dplyr::mutate(.x, dplyr::across(tidyselect::everything(), ~ stringr::str_remove_all(.x, "[[:punct:]]")))) %>%
+    purrr::map(~ dplyr::mutate(.x, dplyr::across(tidyselect::everything(), ~ dplyr::na_if(.x, "")))) %>%
+    purrr::imap(\(x, idx) dplyr::mutate(x, name = stringr::str_to_upper(idx))) %>%
+    purrr::reduce(merge, all = TRUE)
+
+  sublist_mpobj <- list_mpobj %>%
+    purrr::discard(purrr::map_vec(errors, ~ length(.x) > 0))
   
+  # Extract parameters, confidence intervals, variances and Wald test -----------------------------
+  param <- sublist_mpobj %>%
+    purrr::map(pluck, "results", "parameters", std, .default = NULL)
+
+  ci <- sublist_mpobj %>%
+    purrr::map(pluck, "results", "parameters", glue::glue("ci.{std}"), .default = NULL)
+
+  wt <- sublist_mpobj %>%
+    purrr::map(pluck, "results", "summaries", .default = NULL)
+    
+  var <- sublist_mpobj %>%
+    purrr::map(pluck, "results", "sampstat", "univariate.sample.statistics", .default = NULL) %>%
+    purrr::map(as.data.frame)
+  
+  # probs <- sublist_mpobj %>%
+  #   purrr::map(pluck, "results", "parameters", "probability.scale", .default = NULL) %>%
+  #   purrr::map2_dfr(., names(.), ~ if (!is.null(.x)) {
+  #     dplyr::mutate(.x, name = .y)
+  #   }) %>%
+  #   dplyr::mutate(., low2.5 = est - 1.96 * se, up2.5 = est + 1.96 * se) %>%
+  #   dplyr::mutate(dplyr::across(c("est", "pval", "low2.5", "up2.5"), .names = "PRs_{.col}"), .keep = "unused") %>%
+  #   dplyr::mutate(param = paste0(param, "$", category), .keep = "unused") %>%
+  #   dplyr::mutate(name = stringr::str_to_upper(name)) %>%
+  #   dplyr::select(-se, -est_se)
+
+  results <- list(param, ci, wt, var) %>%
+    purrr::map(~ purrr::imap(.x, \(x, idx) dplyr::mutate(x, name = stringr::str_to_upper(idx)))) %>% 
+    purrr::pmap(\(w, x, y, z) purrr::reduce(list(w, x, y, z), ~ merge(.x, .y,  all = TRUE))) # plyr::rbind.fill()
+
+  # Compute odds ratios -----------------------------------------------------
+  OR <- results %>%
+    purrr::keep(stringr::str_detect(purrr::map(sublist_mpobj, ~ purrr::pluck(.x, "VARIABLE")), "CATEGORICAL")) %>%
+    purrr::map(~ dplyr::mutate(
+      .x,
+      dplyr::across(c(est, low2.5, up2.5),
+        ~ ifelse(stringr::str_detect(param, "DIFF"),
+          exp(-(.x)),
+          NA
+        ),
+        .names = "OR_{.col}"
+      )
+    )) %>%
+    purrr::map(~ dplyr::mutate(.x, OR_low2.5 = OR_up2.5, OR_up2.5 = .$OR_low2.5))
+
+  # Compute standardized mean difference ------------------------------------
+  SMD <- results %>%
+    purrr::discard(stringr::str_detect(purrr::map(sublist_mpobj, ~ purrr::pluck(.x, "VARIABLE")), "CATEGORICAL")) %>%
+    purrr::map(~ dplyr::mutate(.x, dplyr::across(tidyselect::where(is.character), ~ dplyr::na_if(.x, "*********")))) %>%
+    purrr::map(~ dplyr::mutate(.x, dplyr::across(c(est, low2.5, up2.5),
+      ~ ifelse(stringr::str_detect(param, "DIFF"),
+        as.numeric(.x) / sqrt(Variance),
+        NA
+      ),
+      .names = "SMD_{.col}"
+    )))
+
+  OR_SMD <- c(OR, SMD) %>%
+    purrr::imap(\(x, idx) dplyr::mutate(x, param = ifelse(paramHeader == "New.Additional.Parameters", paste(stringr::str_to_upper(idx), param), param))) %>%
+    purrr::reduce(merge, all = TRUE)
+
   # Merge each data frame into one table ------------------------------------
-  table <- list(param_ci, probs, wt, warnings, errors) %>%
-    purrr::compact() %>%
-    purrr::reduce(merge, all = TRUE) %>%
-    tryCatch(
-      expr = 
-        dplyr::filter(., paramHeader == "Thresholds" | paramHeader == "New.Additional.Parameters" |
-          (paramHeader == "Means" & stringr::str_detect(param, "[:alpha:]#\\d", negate = TRUE))) %>%
-        dplyr::mutate(sig = dplyr::case_when(
-          WaldChiSq_PValue < 0.001 ~ "***", 
-          WaldChiSq_PValue < 0.01 ~ "**", 
-          WaldChiSq_PValue < 0.05 ~ "*")) %>% 
-        dplyr::mutate(dplyr::across(c(tidyselect::where(is.numeric), -pval, -dplyr::ends_with("PValue")), ~ round(.x, digits = 2))) %>%
-        dplyr::select(LatentClass, param, paramHeader, est, se, pval, 
-                      low2.5, up2.5, dplyr::starts_with("PRs"), dplyr::starts_with("Wald"), sig, warnings, errors) %>%
-        dplyr::arrange(LatentClass, -est, paramHeader, param),
-      error = function(e)
-        .
-    )
-  
+  table <- list(OR_SMD, warn_err) %>%
+    purrr::reduce(full_join, by = "name") %>%
+    dplyr::mutate(param = dplyr::coalesce(param, name)) %>%
+    dplyr::filter(!is.na(errors) | paramHeader == "New.Additional.Parameters" | (paramHeader == "Means" & stringr::str_detect(param, "[:alpha:]#\\d", negate = TRUE))) %>%
+    dplyr::mutate(sig = dplyr::case_when(
+      WaldChiSq_PValue < 0.001 ~ "***",
+      WaldChiSq_PValue < 0.01 ~ "**",
+      WaldChiSq_PValue < 0.05 ~ "*"
+    )) %>%
+    dplyr::mutate(dplyr::across(c(tidyselect::where(is.numeric), -pval, -dplyr::ends_with("PValue")), ~ round(.x, digits = 2))) %>%
+    dplyr::select(LatentClass, param, paramHeader, est, se, pval, low2.5, up2.5, dplyr::starts_with("SMD"), dplyr::starts_with("OR"), dplyr::starts_with("Wald"), sig, warnings, errors) %>%
+    dplyr::arrange(param, LatentClass, desc(est), paramHeader)
+
   return(table)
-  
 }
