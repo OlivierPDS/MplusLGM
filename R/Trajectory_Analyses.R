486819d276e2bbd95e26395cc268e708755af48f
OlivierPDS <olivier.perciedusert@mail.mcgill.ca>
2024-01-25 10:30:57 -0500

    Update Trajectory_Analyses.R
    - edit var of interest
    - edit covariates
    - edit plots

--- R/Trajectory_Analyses.R
+++ R/Trajectory_Analyses.R
@@ -99,16 +99,23 @@ SUMD <- c(SUMD1, SUMD2, SUMD3)
 ## Suicide
 SCD <- paste('cd8', c(0, 1, 2, 3, 6, 9, 12, 18, 24), sep = '_')
 
+## Summary
+SX_0 <- paste(c('SAPS', 'SANS', 'HAS', 'CDS', 'cd8', 'YMRS','SUMD1'), '0', sep = '_')
+SX_24 <- paste(c('SAPS', 'SANS', 'HAS', 'CDS', 'cd8', 'YMRS','SUMD1'), '24', sep = '_')
+SX <- c(SAPS, SANS, HAS, CDS, SCD, YMRS, SUMD)
+
 ## Remission & Recovery
 PSR <- paste('PSR', c(0, 1, 2, 3, 6, 9, 12, 18, 24), sep = '_')
 NSR <- paste('NSR', c(0, 1, 2, 3, 6, 9, 12, 18, 24), sep = '_')
 JSR <- paste('JSR', c(0, 1, 2, 3, 6, 9, 12, 18, 24), sep = '_')
 SR_0 <- paste(c('PSR', 'NSR'), '0', sep = '_')
-SR_24 <- paste(c('PSR', 'NSR', 'JSR'), '24', sep = '_')
 SR_t <- paste(c('PSR', 'NSR', 'JSR'), 't', sep = '_')
 SR_1st <- paste(c('PSR', 'NSR', 'JSR'), '1st', sep = '_')
-SR_cat <- c(PSR, NSR, JSR, 'JSR_by3', 'RECOV_24')
-SR <- c(SR_cat, SR_t, SR_1st, 'JSR_24C')
+SR_24 <- paste(c('PSR', 'NSR', 'JSR'), '24', sep = '_')
+SR_24C <- paste(c('PSR', 'NSR', 'JSR'), '24C', sep = '_')
+RSWG <- c('PSR', 'NSR', 'JSR_by3', 'JSR_at3', 'JSR_at6', 'JSR', 'FR', 'RECOV')
+SR_cat <- c(PSR, NSR, JSR, RSWG)
+SR <- c(SR_cat, SR_t, SR_1st,SR_24C)
 
 ## Medication
 adh <- paste('comp', c(0, 1, 2, 3, 6, 9, 12, 18, 24), sep = '_')
@@ -125,15 +132,11 @@ RX <-  c(ord, adh, CPZ)
 # C <- c('C_SOFAS', 'C_SAPS', 'C_SANS')
 # CP <- c('CP1_SOFAS', 'CP2_SOFAS', 'CP1_SAPS', 'CP2_SAPS', 'CP1_SANS', 'CP2_SANS', 'CP3_SANS')
 
-## Summary
-SX_0 <- paste(c('SAPS', 'SANS', 'HAS', 'CDS', 'cd8', 'YMRS','SUMD1'), '0', sep = '_')
-SX_24 <- paste(c('SAPS', 'SANS', 'HAS', 'CDS', 'cd8', 'YMRS','SUMD1'), '24', sep = '_')
-SX <- c(SAPS, SANS, HAS, CDS, SCD, YMRS, SUMD)
 
 ## covariates
-cov_R3STEP <- c(SD_cat, SD_num, 'ageonset', 'duponset', 'mode', 'ip_op', 'dx_b2', 'SUD', 'PAS_tot2', 'SOFAS_0', COGSTATE, SX_0, SR_0, SR_1st, 'JSR_by3', "comp_0", "CPZ_0") %>% setdiff(c('cd8_0')) #CAYRconv, txm0co, SUMD2_0, SUMD3_0
+cov_R3STEP <- c(SD_cat, SD_num, 'ageonset', 'duponset', 'mode', 'ip_op', 'dx_b2', 'SUD', 'PAS_tot2', 'SOFAS_0', COGSTATE, SX_0, SR_1st, 'JSR_by3', 'JSR_at3', 'JSR_at6', "comp_0", "CPZ_0") %>% setdiff(c('cd8_0', YMRS)) #CAYRconv, txm0co, SUMD2_0, SUMD3_0
 cov_MixReg <- c(SOFAS, SX, PSR, NSR, JSR)
-cov_D3STEP <- c('SOFAS_24', SX_24, SR_24, 'JSR_24C','RECOV_24', "comp_24", "CPZ_24", "txm24co") %>% setdiff(c('cd8_24')) #txm24co
+cov_D3STEP <- c('SOFAS_24', SX_24, RSWG, "comp_24", "CPZ_24", "txm24co") %>% setdiff(c('cd8_24', "JSR_by3", "JSR_at3", "JSR_at6", YMRS)) #txm24co
 
 ## Miscellaneous
 # t <- paste0('t', c(0, 1, 2, 3, 6, 9, 12, 18, 24))
@@ -162,14 +165,6 @@ SD_df <- PEPP2_df %>%
   mutate(dx_b2 = fct_recode(dx_b2, NULL = "substance-induced psychosis")) %>% 
   mutate(dx_b2 = fct_drop(dx_b2))
 
-# ITT_df <- PEPP2_df %>% 
-#   filter(pin <= 857) %>% 
-#   select('pin', all_of(c(cov_R3STEP, cov_MixReg, cov_D3STEP)), starts_with('miss')) %>% 
-#   mutate(across(any_of(cat), ~ to_factor(.x, levels = 'labels'))) %>% #Recode labelled variables as factor 
-#   mutate(across(c(-any_of(cat), -where(is.Date)), ~ as.numeric(as.character(.)))) %>% #Recode labelled variables as numeric
-#   mutate(dx_b2 = fct_drop(dx_b2)) %>% 
-#   mutate(gender = fct_collapse(gender, male = 'Male', female = c('Female', 'other')))
-  
 # SOFAS -------------------------------------------------------------------
 rm(list = ls())
 ## Load workingspace 
@@ -334,7 +329,13 @@ PEPP2_df <- FINAL_model %>%
   merge(PEPP2_df, .,  all = TRUE, by.x ='pin', by.y ='PIN_SOFAS')
 
 ## Step 8: Plot trajectories -----------------------------------------------
-plot <- plotGrowthMixtures(FINAL_model, bw = TRUE, rawdata = TRUE, time_scale = c(0, 12, 24))
+plot <- plotGrowthMixtures(FINAL_model, bw = FALSE, rawdata = TRUE, time_scale = c(0, 12, 24))
+
+plot_GBTM <- map(GBTM_models, 
+                 ~ plotGrowthMixtures(.x, bw = TRUE, rawdata = TRUE, time_scale = c(0, 12, 24)))
+
+plot_BEST <- map(list(GBTM_best, LCGA_best, GMMci_best, GMMcv_best),
+                 ~ plotGrowthMixtures(.x, bw = TRUE, rawdata = TRUE, time_scale = c(0, 12, 24)))
 
 ## Step 9: Covariates -------------------------------------------------
 ## The 3-Step Procedure - multinomial logistic regressions with predictors of class membership
@@ -348,9 +349,9 @@ R3STEP_models <- R3STEP(
 )
 
 R3STEP_unstd <- R3STEPfit(R3STEP_models, std = 'unstd', ref = 1)
-  # filter(pval < (0.05 / length(R3STEP_models))) %>%
+  # filter(pval <= (0.05 / length(R3STEP_models))) %>%
   # filter(str_detect(paramHeader, "C#\\d.ON")) %>%
-  # filter(is.na(errors)) %>% 
+  # filter(is.na(errors)) %>%
   # select(-warnings, -errors)
 
 ## The 3-Step Procedure - mixture regressions with predictors of growth factors 
@@ -361,8 +362,8 @@ D3STEP_models <- D3STEP(
   df = SOFAS_df,
   idvar = 'pin',
   usevar = SOFAS,
-  cov = setdiff(cov_D3STEP, c('SOFAS_24')),
-  startval = list(CDS_24 = 128000, comp_24 = 500, JSR_24C = 128000),
+  cov = setdiff(cov_D3STEP, c('SOFAS_24', 'FR', 'RECOV')),
+  startval = list(comp_24 = 500), #JSR_24C = 128000, CDS_24 = 128000
   model = FINAL_model
 )
 
@@ -612,7 +613,7 @@ BEST_param_dist <- BEST_model_dist %>%
   filter(str_detect(paramHeader, 'Means|^Variances'))
 
 ## Step 8: Plot trajectories -----------------------------------------------
-plot <- plotGrowthMixtures(FINAL_model, bw = TRUE,  rawdata = TRUE, time_scale = c(0, 1, 2, 3, 6, 9, 12, 18, 24))
+plot <- plotGrowthMixtures(FINAL_model, bw = FALSE,  rawdata = TRUE, time_scale = c(0, 1, 2, 3, 6, 9, 12, 18, 24))
 plotGrowthMixtures(OPTIMAL_model, bw = TRUE,  rawdata = TRUE, time_scale = c(0, 1, 2, 3, 6, 9, 12, 18, 24))
 
 ## Step 9: Covariates -------------------------------------------------
@@ -623,32 +624,32 @@ plotGrowthMixtures(OPTIMAL_model, bw = TRUE,  rawdata = TRUE, time_scale = c(0, 
      idvar = 'pin',
      usevar = SAPS,
      cov = setdiff(cov_R3STEP, c('SAPS_0')),
-     model = OPTIMAL_model
-   )
-   
-   R3STEP_unstd <- R3STEPfit(R3STEP_models, std = 'unstd', ref = 1)  %>% 
-     filter(pval < (0.05 / length(R3STEP_models))) %>%
-     filter(str_detect(paramHeader, "C#\\d.ON")) %>%
-     filter(is.na(errors)) %>%
-     select(-warnings, -errors)
-
-# The 3-Step Procedure - mixture regressions with predictors of growth factors
-   MixREG_models <- MixREG(
-     df = SAPS_df,
-     idvar = 'pin',
-     usevar = SAPS, 
-     cov = list(SOFAS, SANS, YMRS, HAS, CDS, SCD, SUMD1, SUMD3, NSR, JSR),
-     # startval = list(SOFAS = 1000, SCD = 1000, YMRS = 1000),
-     gf = "S",
-     gfw = c("iw", "sw"),
-     test = "S_sw",
      model = FINAL_model
    )
-  
-   MixREG_stdyx_cov <- MixREGfit(MixREG_models, std = 'stdyx') %>% 
-     filter(is.na(warnings) & is.na(errors)) %>% 
-     filter(WaldChiSq_PValue < (0.05 / length(MixREG_models))) %>%
-     select(-warnings, -errors)
+   
+   R3STEP_unstd <- R3STEPfit(R3STEP_models, std = 'unstd', ref = 1)
+     # filter(pval < (0.05 / length(R3STEP_models))) %>%
+     # filter(str_detect(paramHeader, "C#\\d.ON")) %>%
+     # filter(is.na(errors)) %>%
+     # select(-warnings, -errors)
+
+# # The 3-Step Procedure - mixture regressions with predictors of growth factors
+#    MixREG_models <- MixREG(
+#      df = SAPS_df,
+#      idvar = 'pin',
+#      usevar = SAPS, 
+#      cov = list(SOFAS, SANS, YMRS, HAS, CDS, SCD, SUMD1, SUMD3, NSR, JSR),
+#      # startval = list(SOFAS = 1000, SCD = 1000, YMRS = 1000),
+#      gf = "S",
+#      gfw = c("iw", "sw"),
+#      test = "S_sw",
+#      model = FINAL_model
+#    )
+#   
+#    MixREG_stdyx_cov <- MixREGfit(MixREG_models, std = 'stdyx') %>% 
+#      filter(is.na(warnings) & is.na(errors)) %>% 
+#      filter(WaldChiSq_PValue < (0.05 / length(MixREG_models))) %>%
+#      select(-warnings, -errors)
  
    
 ## The 3-Step Procedure - Linear regressions with distal outcome
@@ -656,7 +657,7 @@ plotGrowthMixtures(OPTIMAL_model, bw = TRUE,  rawdata = TRUE, time_scale = c(0, 
      df = SAPS_df,
      idvar = 'pin',
      usevar = SAPS,
-     cov = setdiff(cov_D3STEP, c('SAPS_24')),
+     cov = setdiff(cov_D3STEP, c('SAPS_24', 'PSR', 'JSR', 'RECOV')),
      startval = list(comp_24 = 1000),
      model = FINAL_model
    )
@@ -868,8 +869,9 @@ PEPP2_df <- FINAL_model %>%
 ## Step 8: Plot trajectories -----------------------------------------------
 mplus.view.plots(file.choose())
 
-plot <- plotGrowthMixtures(FINAL_model, bw = TRUE, rawdata = TRUE, time_scale = c(0, 1, 2, 3, 6, 9, 12, 18, 24)) 
-+ geom_hline(yintercept = 8,  linetype = "dashed", color = "red")
+plot <- plotGrowthMixtures(FINAL_model, bw = FALSE, rawdata = TRUE, time_scale = c(0, 1, 2, 3, 6, 9, 12, 18, 24))
+
+plot + geom_hline(yintercept = 8,  linetype = "dashed", color = "red")
 
 ## Step 9: Add covariates -------------------------------------------------
 ## The 3-Step Procedure - multinomial logistic regressions with predictors of class membership
@@ -883,36 +885,36 @@ R3STEP_models <- R3STEP(
 )
 
 R3STEP_unstd <- R3STEPfit(R3STEP_models, std = 'unstd', ref = 3)
-  # filter(pval < (0.05 / length(R3STEP_models))) %>%
-  # filter(str_detect(paramHeader, "C#\\d.ON")) %>% 
+  # filter(pval <= (0.05 / length(R3STEP_models))) %>%
+  # filter(str_detect(paramHeader, "C#\\d.ON")) %>%
   # filter(is.na(errors)) %>%
   # select(-warnings, -errors)
 
-# The 3-Step Procedure - mixture regressions with predictors of growth factors
-MixREG_models <- MixREG(
-  df = SANS_df,
-  idvar = 'pin',
-  usevar = SANS,
-  cov = list(SOFAS, SAPS, HAS, CDS, SCD, YMRS, SUMD1, SUMD2, PSR, JSR),
-  startval = list(SOFAS = 500, HAS = 500, CDS = 500, SCD = 500, SUMD1 = 500, SUMD2 = 500, PSR = 500, JSR = 500),
-  gf = "S",
-  gfw = c("iw", "sw"),
-  test = "S_sw",
-  model = FINAL_model, 
-)
-
-MixREG_stdyx <- MixREGfit(MixREG_models, std = 'stdyx') %>% 
-  filter(WaldChiSq_PValue < (0.05 / length(MixREG_models))) %>%
-  filter(is.na(warnings) & is.na(errors)) %>% 
-  select(-warnings, -errors)
+# # The 3-Step Procedure - mixture regressions with predictors of growth factors
+# MixREG_models <- MixREG(
+#   df = SANS_df,
+#   idvar = 'pin',
+#   usevar = SANS,
+#   cov = list(SOFAS, SAPS, HAS, CDS, SCD, YMRS, SUMD1, SUMD2, PSR, JSR),
+#   startval = list(SOFAS = 500, HAS = 500, CDS = 500, SCD = 500, SUMD1 = 500, SUMD2 = 500, PSR = 500, JSR = 500),
+#   gf = "S",
+#   gfw = c("iw", "sw"),
+#   test = "S_sw",
+#   model = FINAL_model, 
+# )
+# 
+# MixREG_stdyx <- MixREGfit(MixREG_models, std = 'stdyx') %>% 
+#   filter(WaldChiSq_PValue < (0.05 / length(MixREG_models))) %>%
+#   filter(is.na(warnings) & is.na(errors)) %>% 
+#   select(-warnings, -errors)
 
 ## The 3-Step Procedure - Linear regressions with distal outcome
 D3STEP_models <- D3STEP(
   df = SANS_df,
   idvar = 'pin',
   usevar = SANS,
-  cov = setdiff(cov_D3STEP, c('SANS_24')),
-  startval = list(CDS_24 = 500, comp_24 = 500, CPZ_24 = 1000, SAPS_24 = 500, SUMD1_24 = 500, YMRS_24 = 500, JSR_24C = 500, HAS_24 = 1000),
+  cov = setdiff(cov_D3STEP, c('SANS_24', 'NSR', 'JSR', 'RECOV')),
+  startval = list(CDS_24 = 16000, comp_24 = 16000, SAPS_24 = 16000, SUMD1_24 = 16000, HAS_24 = 1000, CPZ_24 = 1000),
   model = FINAL_model
 )
 
@@ -964,13 +966,16 @@ FitIndices_GBTM_tscores <- mplusModeler(
 save.image(glue(getwd(), 'SANS', '689', 'SANS_689_{today()}.RData', .sep = '/'))
 
 # Work in Progress --------------------------------------------------------
+prepareMplusData(SOFAS_df, 
+                 filename = "~/Desktop/LGM/SOFAS/Results/R3STEP/duponset_R3STEP.dat")
+
 WIP <- runModels(
-  target = "~/Desktop/LGM/SAPS/Results/FINAL/DIST_SKEWNORMAL.inp",
+  target = "~/Desktop/LGM/SOFAS/Results/R3STEP/duponset_R3STEP.inp",
   showOutput = TRUE,
   logFile = NULL,
 )
 
-WIP_model <- readModels(target = "~/Desktop/LGM/SAPS/Results/FINAL/DIST_SKEWNORMAL.out")
+WIP_model <- readModels(target = "~/Desktop/LGM/SOFAS/Results/R3STEP/duponset_R3STEP.out")
 
 assumptions <- function(models, cov){
 
